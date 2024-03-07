15b4fd9e71053ddbdeedb21a426d5b0cae94d1ab
Sir1ys <sashakoroviy@gmail.com>
2024-03-07 21:50:37 +0000

    first test is passed for the case when rover moves just forward

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function marsRover() {
-  return 0;
+function marsRover(direction) {
+  let xDir = 0;
+  let yDir = 0;
+
+  for (let i = 0; i < direction.length; i++) {
+    if (direction[i] === "M") {
+      yDir += 1;
+    }
+  }
+
+  return `${xDir}:${yDir}:N`;
 }
 
 module.exports = marsRover;
--- intex.test.js
+++ intex.test.js
@@ -1,7 +1,10 @@
 const marsRover = require("./index");
 
 describe("marsRover", () => {
-  it("return correct position when move is passed", () => {
+  it("return correct position when move forward is passed", () => {
     expect(marsRover("M")).toBe("0:1:N");
+    expect(marsRover("MM")).toBe("0:2:N");
+    expect(marsRover("MMM")).toBe("0:3:N");
+    expect(marsRover("MMMM")).toBe("0:4:N");
   });
 });
